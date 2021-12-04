cd7a063dfbc84584c9a6350d7eafdf803976b318
Max <21971014+ToTheMax@users.noreply.github.com>
2021-12-04 15:45:05 +0100

    fix reject bug and ran pretifier

--- main.js
+++ main.js
@@ -11,9 +11,15 @@ const { exit } = require("process");
 // PARSE ARGUMENTS
 const program = new Command();
 program
-    .description("A script to download Snapchat Memories\n\nExample:\n  node main.js -c 50 -f ./json/memories_history.json -o Downloads")
+    .description(
+        "A script to download Snapchat Memories\n\nExample:\n  node main.js -c 50 -f ./json/memories_history.json -o Downloads"
+    )
     .option("-c <number>", "Number of concurrent downloads", 30)
-    .option("-f <path>", "Filepath to memories_history.json", "./json/memories_history.json")
+    .option(
+        "-f <path>",
+        "Filepath to memories_history.json",
+        "./json/memories_history.json"
+    )
     .option("-o <directory>", "Download directory", "Downloads");
 program.parse();
 const options = program.opts();
@@ -21,15 +27,20 @@ const options = program.opts();
 // CONFIG
 const outputDir = options.o;
 const maxConcurrentDownloads = options.c;
-const jsonFile = (!options.f.startsWith("/") && !options.f.startsWith("./")) ? "./" + options.f : options.f;
 const progressBarLength = 20;
+const jsonFile =
+    !options.f.startsWith("/") && !options.f.startsWith("./")
+        ? "./" + options.f
+        : options.f;
 
 // INIT
 try {
     var downloads = require(jsonFile)["Saved Media"];
 } catch (e) {
-    if (e.code === 'MODULE_NOT_FOUND') {
-        console.log("Couldn't find the file " + jsonFile + ", did you provide the right filepath?");
+    if (e.code === "MODULE_NOT_FOUND") {
+        console.log(
+            `Couldn't find the file ${jsonFile}, did you provide the right filepath?`
+        );
         exit();
     }
     throw e;
@@ -37,138 +48,130 @@ try {
 var queue = new Queue(maxConcurrentDownloads);
 var progress = new Progress(downloads.length, progressBarLength);
 
-
 function main() {
-
     // Create download directory
     if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
 
     // Start downloads
     for (let i = 0; i < downloads.length; i++) {
-
-        let [url, body] = downloads[i]["Download Link"].split('?', 2);
-        const fileTime = moment.utc(downloads[i]["Date"], "YYYY-MM-DD HH:mm:ss Z")
+        let [url, body] = downloads[i]["Download Link"].split("?", 2);
+        const fileTime = moment.utc(
+            downloads[i]["Date"],
+            "YYYY-MM-DD HH:mm:ss Z"
+        );
         let fileName = getFileName(downloads[i], fileTime);
 
         // First get CDN download link
-        queue.enqueue(() => getDownloadLink(url, body, fileName, fileTime))
+        queue
+            .enqueue(() => getDownloadLink(url, body, fileName, fileTime))
             .then((result) => {
                 progress.cdnLinkSucceeded(true);
 
                 // Download the file
-                queue.enqueue(() => downloadMemory(result[0], result[1], result[2]))
+                queue
+                    .enqueue(() =>
+                        downloadMemory(result[0], result[1], result[2])
+                    )
                     .then((success) => {
                         progress.downloadSucceeded(true);
                     })
                     .catch((err) => {
                         progress.downloadSucceeded(false);
-                    })
+                    });
             })
             .catch((err) => {
                 progress.cdnLinkSucceeded(false);
-            })
+            });
     }
 }
 
 function getFileName(download, fileTime) {
-    var filename = fileTime.format("YYYY-MM-DD_HH-mm-ss")
+    var fileName = fileTime.format("YYYY-MM-DD_HH-mm-ss");
 
-    if (download["Media Type"] == "PHOTO")
-        filename += ".jpg";
-    else if (download["Media Type"] == "VIDEO")
-        filename += ".mp4";
+    if (download["Media Type"] == "PHOTO") fileName += ".jpg";
+    else if (download["Media Type"] == "VIDEO") fileName += ".mp4";
 
-    return filename
+    return fileName;
 }
 
-const getDownloadLink = (url, body, filename, fileTime) => new Promise(resolve => {
-    var parsedUrl = new URL(url);
-    const options = {
-        hostname: parsedUrl.hostname,
-        path: parsedUrl.pathname,
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-    };
+const getDownloadLink = (url, body, fileName, fileTime) =>
+    new Promise((resolve, reject) => {
+        var parsedUrl = new URL(url);
+        const options = {
+            hostname: parsedUrl.hostname,
+            path: parsedUrl.pathname,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        };
 
-    var req = https.request(options, (res) => {
-
-        data = "";
-        res.on("data", (chunk) => {
-            data += chunk;
+        var req = https.request(options, (res) => {
+            data = "";
+            res.on("data", (chunk) => {
+                data += chunk;
+            });
+            res.on("end", function () {
+                if (res.statusCode == 200) {
+                    resolve([data, fileName, fileTime]);
+                } else {
+                    reject("status error");
+                }
+            });
         });
-        res.on("error", function () {
+        req.on("error", function () {
+            req.destroy();
             reject("request error");
-        })
-        res.on("end", function () {
-            if (res.statusCode == 200) {
-                resolve([data, filename, fileTime]);
-            }
-            else {
-                reject("status error");
-            }
         });
+
+        req.write(body);
+        req.end();
     });
-    req.write(body);
-    req.end();
-});
 
-
-const downloadMemory = (downloadUrl, filename, fileTime) => new Promise(resolve => {
-
-    // Check if there already exists a file with the same name/timestamp
-    if (fs.existsSync(outputDir + "/" + filename)) {
-        duplicates = 1;
-        while (true) {
-            var extensionPos = filename.lastIndexOf('.');
-            var newFilename = filename.substring(0, extensionPos) + " (" + duplicates + ")" + filename.substring(extensionPos, filename.length);
-            if (fs.existsSync(outputDir + "/" + newFilename))
-                duplicates++;
-            else {
-                filename = newFilename;
-                break;
+const downloadMemory = (downloadUrl, fileName, fileTime) =>
+    new Promise((resolve, reject) => {
+        // Check if there already exists a file with the same name/timestamp
+        if (fs.existsSync(outputDir + "/" + fileName)) {
+            duplicates = 1;
+            while (true) {
+                var extensionPos = fileName.lastIndexOf(".");
+                var newFilename =
+                    fileName.substring(0, extensionPos) +
+                    " (" +
+                    duplicates +
+                    ")" +
+                    fileName.substring(extensionPos, fileName.length);
+                if (fs.existsSync(outputDir + "/" + newFilename)) duplicates++;
+                else {
+                    fileName = newFilename;
+                    break;
+                }
             }
         }
-    }
 
-    // Create the file and write to it
-    var file = fs.createWriteStream(outputDir + "/" + filename);
-
-    https.get(downloadUrl, function (res) {
-        res.pipe(file);
-        res.on("error", function () {
-            reject("request error");
-        })
-        res.on("end", function () {
-            file.close();
+        var req = https.get(downloadUrl, function (res) {
             if (res.statusCode == 200) {
-                // Update the file creation date
-                utimes(file.path, {
-                    btime: fileTime.valueOf(), // birthtime (Windows & Mac)
-                    mtime: fileTime.valueOf() // modified time (Windows, Mac, Linux)
-                }).then(() => {
-                    resolve(true)
-                }).catch(err => {
-                    reject("failed to set file creation date:", err)
-                })
-            }
-            else {
+                // Create the file and write to it
+                var file = fs.createWriteStream(outputDir + "/" + fileName);
+                res.pipe(file);
+                res.on("end", function () {
+                    file.close();
+                    // Update the file creation date
+                    utimes(file.path, {
+                        btime: fileTime.valueOf(), // birthtime (Windows & Mac)
+                        mtime: fileTime.valueOf(), // modified time (Windows, Mac, Linux)
+                    });
+                    resolve(true);
+                });
+            } else {
                 reject("status error");
             }
         });
+        req.on("error", function () {
+            req.destroy();
+            reject("request error");
+        });
+        req.end();
     });
-});
 
 main();
-
-
-// Don"t look at this
-process.on("uncaughtException", function (err) {
-    if (err.code == "ECONNRESET")
-        progress.downloadSucceeded(false);
-    else{
-        console.error(err, 'Uncaught Exception thrown');
-        process.exit(1);
-    }
-});
